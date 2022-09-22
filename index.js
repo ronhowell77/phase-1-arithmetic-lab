9c9db1a49ed54e118e53d390f0dbfb77ee5bbcdc
Ronnie <ron.howell0326@gmail.com>
2022-09-22 18:37:17 -0400

    commit tree f7c9daf0fd4e76ea2699d8b5b800b89b2d7a5393

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 9
+let num4 = 31
+let mod = num4 % num3
+let max = Math.max(5, 10, 15, 20);
 // Write your code here
